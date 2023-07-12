d165e34f633aa77afa35889cdb3783502ed2a19d
Samuel ELGRABLY <selgrabl@paul-f4Br4s1.clusters.42paris.fr>
2023-07-12 14:34:09 +0200

    fix the fix

--- P1/build.sh
+++ P1/build.sh
@@ -1,2 +1,2 @@
-docker build -t host_selgrabl -f ./host_dockerfile/. .
-docker build -t router_selgrabl	-f ./router_dockerfile/. .
+docker build -t host_selgrabl -f ./host_dockerfile/Dockerfile .
+docker build -t router_selgrabl	-f ./router_dockerfile/Dockerfile .
